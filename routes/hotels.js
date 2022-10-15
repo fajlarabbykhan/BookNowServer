fa42a2a13596a6d0c15ec9fda71ab1dbb0107361
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-10-15 23:32:34 +0600

    hotel cerate and update api done

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,7 +1,24 @@
 import express from "express";
+import Hotel from "../models/Hotel.js"
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello from hotels point")
+//create hotels
+router.post("/", async (req, res) => {
+    const newHotel = new Hotel(req.body)
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+//update hotels
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 export default router;

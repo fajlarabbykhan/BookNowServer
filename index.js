4368be84c6926e15118e5f5d8d73e1448f2a6f4e
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-10-15 22:26:59 +0600

    all routes create done

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const { default: mongoose } = require('mongoose');
-require('dotenv').config()
-// "type": "module",
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const port = process.env.PORT || 4000
+const app = express();
+dotenv.config();
 
 const connect = async () => {
     try {
+
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected with Database");
     } catch (error) {
@@ -18,10 +26,16 @@ mongoose.connection.on("disconnected", () => {
 })
 
 
+//middlewares
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/server/auth", authRoute);
+app.use("/server/users", usersRoute);
+app.use("/server/hotels", hotelsRoute);
+app.use("/server/rooms", roomsRoute);
 
 app.listen(port, () => {
     connect();

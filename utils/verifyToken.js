88fc3a9056b6aaf5e1091fed1f4b166ba211ab1f
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-11-22 20:27:37 +0600

    try

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,34 +1,32 @@
 import jwt from "jsonwebtoken";
-import { createError } from "../utils/error.js"
+import { createError } from "../utils/error.js";
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    if (!token) {
-        return next(createError(401, "You have no access"))
-    }
-    jwt.verify(token, process.env.JWT, (err, user) => {
-        if (err) return next(createError(401, "Token is not valid"));
-        req.user = user;
-        next()
-    })
-}
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "You have no access"));
+  }
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(401, "Toke is not valid"));
+    req.user = user;
+    next();
+  });
+};
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) {
-            next()
-        }
-        else {
-            if (err) return next(createError(403, "U r not authorized"))
-        }
-    })
-}
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      if (err) return next(createError(403, "U r not authorized"));
+    }
+  });
+};
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if (req.user.isAdmin) {
-            next()
-        }
-        else {
-            if (err) return next(createError(403, "U r not authorized"))
-        }
-    })
-}
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      if (err) return next(createError(403, "U r not authorized"));
+    }
+  });
+};

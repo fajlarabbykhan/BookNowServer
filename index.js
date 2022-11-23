dfb974fde3da031284d17886b1939cab6ce37a88
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-11-23 09:46:36 +0600

    try

--- index.js
+++ index.js
@@ -8,28 +8,26 @@ import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 const app = express();
 dotenv.config();
 
 const connect = async () => {
-    try {
-
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected with Database");
-    } catch (error) {
-        throw (error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.CONURL);
+    console.log("Connected with Database");
+  } catch (error) {
+    throw error;
+  }
+};
 mongoose.connection.on("disconnected", () => {
-    console.log("Mongodb Disconnected");
-})
-
+  console.log("Mongodb Disconnected");
+});
 
 //middlewares
 
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/server/auth", authRoute);
@@ -38,17 +36,17 @@ app.use("/server/hotels", hotelsRoute);
 app.use("/server/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong!";
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-    });
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
 });
 
 app.listen(port, () => {
-    connect();
-    console.log(`Example app listening on port ${port}`)
-})
+  connect();
+  console.log(`Example app listening on port ${port}`);
+});
